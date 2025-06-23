5090812333b0d615a9b523bfddd568fafb3d63d1
vishalkumarkhatri <vk.khatri2003@gmail.com>
2025-06-23 23:40:30 +0530

    Fixed bugs on profile page

--- Frontend/src/pages/ProfilePage.jsx
+++ Frontend/src/pages/ProfilePage.jsx
@@ -84,7 +84,7 @@ function ProfilePage() {
                         <div className="space-y-3 text-sm">
                             <div className="flex items-center justify-between py-2 border-b border-zinc-700">
                                 <span>Member Since</span>
-                                <span>{authUser?.createdAt?.split('T')[0]}</span>
+                                <span>{authUser?.createdAt.split('T')[0]}</span>
                             </div>
                             <div className="flex items-center justify-between py-2">
                                 <span>Account Status</span>
